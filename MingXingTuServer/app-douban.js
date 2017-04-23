c1fac532341a8b50c419871f570900cb74800023
1029278668@qq.com <1029278668@qq.com>
2017-04-24 00:44:49 +0800

    51MingXingTu
    51MingXingTu

---
+++ MingXingTuServer/app-douban.js
@@ -0,0 +1,95 @@
+var superagent = require('superagent');
+var charset = require('superagent-charset');
+charset(superagent);
+var cheerio = require('cheerio');
+var express = require('express');
+var app = express();
+var https = require('https');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+var urlencodeParser = bodyParser.urlencoded({extend: false});
+
+app.use(express.static('public'));
+
+var baseUrl = 'http://www.dbmeinv.com';
+const successCode = 0, failCode = -1;
+
+function isEmpty(obj){
+    for (let i in obj){
+        return false;
+    }
+    return true;
+}
+
+app.get('/', function(req, res){
+    res.send('<h1>girls now!</h1>');
+});
+
+app.get('/tags', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+    superagent.get(baseUrl)
+    .charset('utf-8')
+    .end(function (err, sres) {
+        var items = [];
+        if (err) {
+            console.log('ERR: ' + err);
+            res.json({code: failCode, msg: err, sets:items});
+            return;
+        }
+        var $ = cheerio.load(sres.text);
+        $('#main .panel-heading ul.nav li a').each(function (idx, element) {
+            var $element = $(element);
+            var hrefStr = $element.attr('href');
+            var cid = hrefStr.match(/cid=(\d)/);
+            cid = isEmpty(cid) ? "0" : cid[1];
+            items.push({
+                title : $element.text(),
+                href : hrefStr,
+                cid : cid,
+            });
+        });
+        res.json({code: successCode, msg: "", data:items});
+    });
+});
+
+app.get('/girls', function(req, res){
+    var cid = req.query.c;
+    var page = req.query.p;
+    console.log("cid: " + cid);
+    console.log("page: " + page);
+    cid = !isEmpty(cid) ? cid : '0';
+    page = !isEmpty(page) ? page : '1';
+    var route = '/dbgroup/show.htm?cid=' + cid + '&pager_offset=' + page;
+    res.header("Content-Type", "application/json; charset=utf-8");
+    superagent.get(baseUrl+route)
+    .charset('utf-8')
+    .end(function (err, sres) {
+        if (err) {
+            console.log('ERR: ' + err);
+            return next(err);
+        }
+        var $ = cheerio.load(sres.text);
+        var items = [];
+        var t1 = new Date().getTime();
+        $('#main .panel-body ul.thumbnails li.span3 .img_single a').each(function (idx, element) {
+            var $element = $(element);
+            var $subElement = $element.find('img.height_min');
+            var thumbImgSrc = $subElement.attr('src');
+            items.push({
+                title : $subElement.attr('title'),
+                href : $element.attr('href'),
+                largeSrc : isEmpty(thumbImgSrc) ? "" : thumbImgSrc.replace('bmiddle', 'large'),
+                thumbSrc : thumbImgSrc,
+                smallSrc : isEmpty(thumbImgSrc) ? "" : thumbImgSrc.replace('bmiddle', 'small'),
+            });
+        });
+        res.json({code: successCode, msg: "", data:items});
+    });
+});
+
+var server = app.listen(8090, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例，访问地址为：http://%s:%s",host,port);
+})
---
+++ README.md
@@ -0,0 +1,38 @@
+# weapp-51Meizhitu
+weapp-51Meizhitu
+
+### 新浪云活动
+
+新浪云平台Sina App Engine(SAE)，是由新浪公司开发和运营的开放云计算平台的核心组成部分，国内第一家公有云计算平台，可以为网站开发者，App开发者提供稳定、快捷、透明、可控的服务化的平台，并且减少开发者的开发和维护成本。
+
+「新浪云福利」1000云豆免费领！低成本、免运维、灵活、安全稳定，轻松应对业务爆发式增长，一起来用吧！ 注册地址：http://t.cn/R5oFIaD
+
+
+### 最新技术福利
+
+免费视频教程百度云盘链接：
+
+React Native入门  链接: https://pan.baidu.com/s/1qYtryC8
+
+ionic入门  链接: https://pan.baidu.com/s/1i5mKcnF
+
+微信小程序入门  链接: https://pan.baidu.com/s/1o8FGjDw
+
+为了保证连接的可使用性，请关注微信公众号 <font color=red>`全栈弄潮儿`</font>，
+
+React Native入门视频 回复“RN提取码”领取 提取码
+
+ionic入门视频 回复“ionic提取码”领取 提取码
+
+微信小程序入门视频 回复“小程序提取码”领取 提取码
+
+
+#### 欢迎关注我的微信公众号或头条号`全栈弄潮儿` ，获取更多学习资源及技术文章等
+
+* 微信公众号二维码，扫一扫或者搜索"全栈弄潮儿"即可关注
+
+<img src="https://github.com/Alex-0407/sinacloud-node/blob/master/fullstack-8cm.jpg" width="320px" style="display:inline;">
+
+* 今日头条号二维码，扫一扫即可关注
+
+<img src="https://github.com/Alex-0407/node-demo/blob/master/toutiao.jpg" width="320px" style="display:inline;">
